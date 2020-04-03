d26f2450152b86cd3a612f20380811f899257018
chris brady <chrisb3546@yahoo.com>
2020-04-03 13:01:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect (empty_array)
     empty_array = []
     i = 0
-     while i < 10
+     while i < empty_array.length
       my_collect(empty_array) do |x|
         counter += 1
       end
